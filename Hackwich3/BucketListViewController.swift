c151eebe44f54ae609ec5306d52acc5c3014cc4f
nicasiap <35237427+nicasiap@users.noreply.github.com>
2018-03-12 15:53:26 -1000

    ATTEMPTED weather View controller

--- Hackwich3/BucketListViewController.swift
+++ Hackwich3/BucketListViewController.swift
@@ -8,20 +8,41 @@
 
 import UIKit
 
-class BucketListViewController: UIViewController {
+class BucketListViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var tableView: UITableView!
+    var bucketListArray = ["Travel the world", "Learn how to speak at least 3 languages fluently", "Publish a book", "Do something I never thought of doing", "Have a large scale display of one of my art pieces"]
+    
+  
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        self.tableView.dataSource = self
         // Do any additional setup after loading the view.
     }
-
+    
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
 
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return bucketListArray.count
+        
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellReuselIdentifier")!
+        let text = bucketListArray[indexPath.row]
+        cell.textLabel?.text = text
+        return cell
+        
+    }
     /*
     // MARK: - Navigation
 
--- Hackwich3/WeatherViewController.swift
+++ Hackwich3/WeatherViewController.swift
@@ -10,26 +10,36 @@ import UIKit
 
 class WeatherViewController: UIViewController {
 
+    @IBOutlet weak var enterValueText: UITextField!
+    @IBOutlet weak var finalValueText: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
 
         // Do any additional setup after loading the view.
     }
-
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func convertButtonPressed(_ sender: UITextField) {
+        let fahrenheit : input.self 
+        
+        
+        print( "Today is /(celsius) degrees" )
+        celsius = (fahrenheit - 32) * (5/9)
+        
     }
-    */
+    
+    
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        enterValueText.resignFirstResponder()
+    }
 
-}
+    }
+
