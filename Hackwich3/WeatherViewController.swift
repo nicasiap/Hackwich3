03859d59b9d05d27ceafedaec02341811a7aaae2
CM Student <cmstudent@UHWO-ACM-C-01.local>
2018-03-15 14:15:12 -1000

    Finished

--- Hackwich3/WeatherViewController.swift
+++ Hackwich3/WeatherViewController.swift
@@ -26,20 +26,18 @@ class WeatherViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    @IBAction func convertButtonPressed(_ sender: UITextField) {
-        let fahrenheit : input.self 
+    @IBAction func convertButtonPressed(_ sender: Any) {
+        var userInput: Float
+        userInput = Float(enterValueText.text!)!
+        var x:String
+        x = String((userInput - 32) * 5/9)
+        self.finalValueText.text = "Today is \(x) degrees."
         
         
-        print( "Today is /(celsius) degrees" )
-        celsius = (fahrenheit - 32) * (5/9)
         
     }
     
     
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        enterValueText.resignFirstResponder()
-    }
 
     }
 
